7c6bfee1c304752d36a8d0eaf82c81ccbc734bf0
Elad Hoffer <ehoffer@gpumonster5.habana-labs.com>
2021-07-22 16:06:18 +0300

    lr schedule

--- src/optim/scheduler.py
+++ src/optim/scheduler.py
@@ -1,9 +1,13 @@
-def linear_warmup_and_decay(lr, warmup_steps, total_steps):
+def linear_warmup_and_decay(lr, warmup_steps, total_steps, return_dict=True):
     lr = float(lr)
 
-    def lr_t(t):
+    def _lr_t_fn(t):
         if t <= warmup_steps:
-            return (t / warmup_steps) * lr
+            lr_t = (t / warmup_steps) * lr
         else:
-            return max(((total_steps - t) / (total_steps - warmup_steps)) * lr, 0.)
-    return lr_t
+            lr_t = max(((total_steps - t) / (total_steps - warmup_steps)) * lr, 0.)
+        if return_dict:
+            return {'lr': lr_t}
+        else:
+            return lr_t
+    return _lr_t_fn
