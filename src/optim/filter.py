c76db0936990dc085db5ff1fc3e4156accda38b4
Elad Hoffer <ehoffer@habana.ai>
2021-12-07 13:38:56 +0200

    filter bn with bias

--- src/optim/filter.py
+++ src/optim/filter.py
@@ -1,6 +1,10 @@
 from src.utils_pt.param_filter import FilterParameters, is_bn
 
 
+def is_bias(name):
+    return name.endswith('bias')
+
+
 class Filter(FilterParameters):
     def __init__(self, model, module=None, module_name=None, parameter_name=None, exclude=False):
         super().__init__(model, module, module_name, parameter_name, exclude)
@@ -9,3 +13,8 @@ class Filter(FilterParameters):
 class OnlyBN(Filter):
     def __init__(self, model, *kargs, **kwargs):
         super().__init__(model, module=is_bn, *kargs, **kwargs)
+
+
+class OnlyBiasBN(Filter):
+    def __init__(self, model, *kargs, **kwargs):
+        super().__init__(model, module=is_bn, parameter_name=is_bias, *kargs, **kwargs)
