1f9bee7c99b6a24d71b1e9d03396a0dbf250a57d
Elad Hoffer <elad.hoffer@gmail.com>
2022-05-25 11:52:17 +0300

    Update utils.py

--- src/models/modules/utils.py
+++ src/models/modules/utils.py
@@ -1,20 +1,28 @@
 import torch
 
 
+def freeze_model_(model):
+    for p in model.parameters():
+        p.requires_grad_(False)
+
+
 class FrozenModule(torch.nn.Module):
     def __init__(self, module):
         super().__init__()
         self.module = module
-        for p in self.module.parameters():
-            p.requires_grad_(False)
+        freeze_model_(self.module)
 
     def forward(self, *args, **kwargs):
         with torch.no_grad():
             return self.module(*args, **kwargs)
 
 
-def remove_module(model, name):
+def replace_module(model, name, new_module):
     for n, m in model.named_children():
         if n == name:
-            setattr(model, name, torch.nn.Identity())
+            setattr(model, name, new_module)
         remove_module(m, name)
+
+
+def remove_module(model, name):
+    replace_module(model, name, torch.nn.Identity())
