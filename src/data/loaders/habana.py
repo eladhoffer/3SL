c1cec652f9d5ded5944b774846f0d6bd83dc1519
Elad Hoffer <ehoffer@habana.ai>
2022-09-21 10:54:09 +0000

    hpu support update

--- src/data/loaders/habana.py
+++ src/data/loaders/habana.py
@@ -3,7 +3,7 @@ from habana_dataloader import HabanaDataLoader as _HabanaDataLoader
 
 class HabanaDataLoader(_HabanaDataLoader):
     def __init__(self, *args, **kwargs):
+        args = [kwargs.pop('dataset')]
         super().__init__(*args, **kwargs)
-        self._aeon_dl_handle_vars(kwargs)
         self.collate_fn = None
         self.worker_init_fn = None
